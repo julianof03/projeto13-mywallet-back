d7254f3660c39d1489779dbfc4a456291f68917a
julianof03 <jlprod08@gmail.com>
2022-09-10 00:03:41 -0300

    baseScrip

---
+++ index.js
@@ -0,0 +1,63 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import joi from "joi";
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+let db;
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+mongoClient.connect().then(() => { db = mongoClient.db("MyWalletdb"); });
+
+const registerSchema = joi.object({
+    name: joi.string().min(1).required(),
+    email: joi.string().min(1).required(),
+    password: joi.string().min(1).required(),
+});
+const loginSchema = joi.object({
+    email: joi.string().min(1).required(),
+    password: joi.string().min(1).required(),
+});
+
+app.post('/register', async (req, res) => {
+
+    const { name, email, password } = req.body;
+    const validateRegister = registerSchema.validate(req.body);
+    if (validateRegister.error) {
+        res.sendStatus(422);
+        return
+    }
+    console.log(name, email, password);
+    
+    const registreUser = await db.collection("participants").findOne({email});
+    if(registreUser){
+         res.status(409).send("Usuario ja existente");
+         return    
+     }
+    try {
+        db.collection("participants").insertOne({
+                name,
+                email,
+                password, 
+                lastStatus: Date.now(),
+            }
+        );
+
+        const participantes = await db.collection("participants").find({}).toArray();
+        res.send(participantes);
+
+    } catch { res.sendStatus(500); }
+
+});
+
+
+
+
+app.listen(5000), () => console.log("Listening on port 5000");
