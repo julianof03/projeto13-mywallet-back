ad149ea2cf5831c045777071c68ce356e3762785
julianof03 <jlprod08@gmail.com>
2022-09-10 19:55:45 -0300

    Get and Post In/Out

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import dayjs from "dayjs";
 import joi from "joi";
 
 
@@ -16,6 +17,8 @@ let db;
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 mongoClient.connect().then(() => { db = mongoClient.db("MyWalletdb"); });
 
+let OnlineUser = 'undefined22';
+
 const registerSchema = joi.object({
     name: joi.string().min(1).required(),
     email: joi.string().min(1).required(),
@@ -24,6 +27,10 @@ const registerSchema = joi.object({
 const loginSchema = joi.object({
     email: joi.string().min(1).required(),
     password: joi.string().min(1).required(),
+});
+const inOutSchema = joi.object({
+    valor: joi.number().min(1).required(),
+    text: joi.string().min(1).required(),
 });
 
 app.post('/register', async (req, res) => {
@@ -51,7 +58,7 @@ app.post('/register', async (req, res) => {
         );
 
         const participantes = await db.collection("participants").find({}).toArray();
-        res.send(participantes);
+        res.send(201);
 
     } catch { res.sendStatus(500); }
 
@@ -66,12 +73,13 @@ app.post('/', async (req, res)=>{
     }
     
     const loginUser = await db.collection("participants").findOne({email});
+    OnlineUser = loginUser.name;
     if(!loginUser){
          res.status(409).send("Usuario não cadastrado");
          return    
      }
     try {
-        db.collection("lo").insertOne({
+        db.collection("participants").insertOne({
                 email,
                 password, 
                 lastStatus: Date.now(),
@@ -83,6 +91,64 @@ app.post('/', async (req, res)=>{
 
     } catch { res.sendStatus(500); } 
 })
+
+app.post('/newinscreen', async (req, res)=>{
+    const {valor, text } = req.body;
+    const validadeInOut = inOutSchema.validate(req.body);
+    const User = OnlineUser;
+    console.log(User);
+    if (validadeInOut.error) {
+        res.sendStatus(422);
+        return
+    }
+    
+    try {
+        db.collection("InOut").insertOne({
+                User,
+                day: dayjs().format("DD/MM"),
+                amount: valor,
+                text: text,
+                type: 'in',
+
+            }
+        );
+        res.sendStatus(201);
+
+    } catch { res.sendStatus(500); } 
+})
+app.post('/newoutscreen', async (req, res)=>{
+    const {valor, text } = req.body;
+    const validadeInOut = inOutSchema.validate(req.body);
+    const User = OnlineUser;
+    console.log(User);
+    if (validadeInOut.error) {
+        res.sendStatus(422);
+        return
+    }
+    
+    try {
+        db.collection("InOut").insertOne({
+                User,
+                day: dayjs().format("DD:MM"),
+                amount: valor,
+                text: text,
+                type: 'out',
+
+            }
+        );
+        res.sendStatus(201);
+
+    } catch { res.sendStatus(500); } 
+})
+app.get('/listscreen', async (req, res)=>{
+    const User = OnlineUser;
+    console.log(User);
+    try {
+        const outputs = await db.collection("InOut").find({User}).toArray();
+        const listarr = outputs;
+        res.send(listarr);
+    } catch { res.sendStatus(500); } 
+});
 
 
 app.listen(5000), () => console.log("Listening on port 5000");
